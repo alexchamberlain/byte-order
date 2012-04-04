dd7ae83d193db9faa217a268f0f00d1288f3314b
Alex Chamberlain <alex@alexchamberlain.co.uk>
2012-04-04 09:16:54 +0100

    Cross platform host to LE/BE support. No testing.

--- byte-order.h
+++ byte-order.h
@@ -2,7 +2,19 @@
 
 
 uint32_t htobel(uint32_t v) {
+  uint32_t ret;
+  uint8_t * data = &ret;
 
+  data[0] = v/(2 << 24);
+  v %= (2 << 24);
+  data[1] = v/(2 << 16);
+  v %= (2 << 16);
+  data[2] = v/(2 <<  8);
+  v %= (2 <<  8);
+  data[3] = v/(2 <<  0);
+  v %= (2 <<  0);
+
+  return ret;
 }
 
 uint32_t betohl(uint32_t v) {
@@ -11,7 +23,23 @@ uint32_t betohl(uint32_t v) {
 }
 
 uint32_t htolel(uint32_t v) {
+  uint32_t ret;
+  uint8_t * data = &ret;
 
+  data[0] = v/(2 << 24);
+  v %= (2 << 24);
+  data[1] = v/(2 << 16);
+  v %= (2 << 16);
+  data[2] = v/(2 <<  8);
+  v %= (2 <<  8);
+  data[3] = v/(2 <<  0);
+  v %= (2 <<  0);
+
+  return ret;
+}
+
+uint32_t betohl(uint32_t v) {
+  uint8_t * data = &v;
 }
 
 uint32_t letohl(uint32_t v) {
