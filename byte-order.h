713b57690abfc6b9f354feaaab48d19216900b42
Alex Chamberlain <alex@alexchamberlain.co.uk>
2012-04-04 09:27:27 +0100

    Inline functions... C99 code.

--- byte-order.h
+++ byte-order.h
@@ -1,7 +1,7 @@
 #include <stdint.h>
 
 
-uint32_t htobel(uint32_t v) {
+inline uint32_t htobel(uint32_t v) {
   uint32_t ret;
   uint8_t * data = &ret;
 
@@ -17,12 +17,12 @@ uint32_t htobel(uint32_t v) {
   return ret;
 }
 
-uint32_t betohl(uint32_t v) {
+inline uint32_t betohl(uint32_t v) {
   uint8_t * data = &v;
   return (data[3]<<0) | (data[2]<<8) | (data[1]<<16) | (data[0]<<24);
 }
 
-uint32_t htolel(uint32_t v) {
+inline uint32_t htolel(uint32_t v) {
   uint32_t ret;
   uint8_t * data = &ret;
 
@@ -38,11 +38,11 @@ uint32_t htolel(uint32_t v) {
   return ret;
 }
 
-uint32_t betohl(uint32_t v) {
+inline uint32_t betohl(uint32_t v) {
   uint8_t * data = &v;
 }
 
-uint32_t letohl(uint32_t v) {
+inline uint32_t letohl(uint32_t v) {
   uint8_t * data = &v;
   return (data[3]<<0) | (data[2]<<8) | (data[1]<<16) | (data[0]<<24);
 }
