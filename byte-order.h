791e574b4cd2c58993be596f6c6185133eb2ce0d
Alex Chamberlain <alex@alexchamberlain.co.uk>
2012-04-04 09:38:41 +0100

    Detect endianess.

--- byte-order.h
+++ byte-order.h
@@ -1,50 +1,93 @@
 #include <stdint.h>
 
+#if defined (__GLIBC__)
+  #include <endian.h>
+  #if (__BYTE_ORDER == __LITTLE_ENDIAN)
+    #define LITTLE_ENDIAN
+  #elif (__BYTE_ORDER == __BIG_ENDIAN)
+    #define BIG_ENDIAN
+  #else
+    #error Unknown machine endianness detected.
+  #endif
+#elif defined(_BIG_ENDIAN)
+  #define BIG_ENDIAN
+#elif defined(_LITTLE_ENDIAN)
+  #define LITTLE_ENDIAN
+#elif defined(__sparc) || defined(__sparc__) \
+   || defined(_POWER) || defined(__powerpc__) \
+   || defined(__ppc__) || defined(__hpux) \
+   || defined(_MIPSEB) || defined(_POWER) \
+   || defined(__s390__)
+  #define BIG_ENDIAN
+#elif defined(__i386__) || defined(__alpha__) \
+   || defined(__ia64) || defined(__ia64__) \
+   || defined(_M_IX86) || defined(_M_IA64) \
+   || defined(_M_ALPHA) || defined(__amd64) \
+   || defined(__amd64__) || defined(_M_AMD64) \
+   || defined(__x86_64) || defined(__x86_64__) \
+   || defined(_M_X64)
+  #define LITTLE_ENDIAN
+#else
+  #error Please file an issue on Github (alexchamberlain/byte-order) with CPU type.
+#endif
+
 
 inline uint32_t htobel(uint32_t v) {
-  uint32_t ret;
-  uint8_t * data = &ret;
+  #ifdef BIG_ENDIAN
+    return v;
+  #else
+    uint32_t ret;
+    uint8_t * data = &ret;
 
-  data[0] = v/(2 << 24);
-  v %= (2 << 24);
-  data[1] = v/(2 << 16);
-  v %= (2 << 16);
-  data[2] = v/(2 <<  8);
-  v %= (2 <<  8);
-  data[3] = v/(2 <<  0);
-  v %= (2 <<  0);
+    data[0] = v/(2 << 24);
+    v %= (2 << 24);
+    data[1] = v/(2 << 16);
+    v %= (2 << 16);
+    data[2] = v/(2 <<  8);
+    v %= (2 <<  8);
+    data[3] = v/(2 <<  0);
+    v %= (2 <<  0);
 
-  return ret;
+    return ret;
+  #endif
 }
 
 inline uint32_t betohl(uint32_t v) {
-  uint8_t * data = &v;
-  return (data[3]<<0) | (data[2]<<8) | (data[1]<<16) | (data[0]<<24);
+  #ifdef BIG_ENDIAN
+    return v;
+  #else
+    uint8_t * data = &v;
+    return (data[3]<<0) | (data[2]<<8) | (data[1]<<16) | (data[0]<<24);
+  #endif
 }
 
 inline uint32_t htolel(uint32_t v) {
-  uint32_t ret;
-  uint8_t * data = &ret;
+  #ifdef LITTLE_ENDIAN
+    return v;
+  #else
+    uint32_t ret;
+    uint8_t * data = &ret;
 
-  data[0] = v/(2 << 24);
-  v %= (2 << 24);
-  data[1] = v/(2 << 16);
-  v %= (2 << 16);
-  data[2] = v/(2 <<  8);
-  v %= (2 <<  8);
-  data[3] = v/(2 <<  0);
-  v %= (2 <<  0);
+    data[0] = v/(2 << 24);
+    v %= (2 << 24);
+    data[1] = v/(2 << 16);
+    v %= (2 << 16);
+    data[2] = v/(2 <<  8);
+    v %= (2 <<  8);
+    data[3] = v/(2 <<  0);
+    v %= (2 <<  0);
 
-  return ret;
-}
-
-inline uint32_t betohl(uint32_t v) {
-  uint8_t * data = &v;
+    return ret;
+  #endif
 }
 
 inline uint32_t letohl(uint32_t v) {
-  uint8_t * data = &v;
-  return (data[3]<<0) | (data[2]<<8) | (data[1]<<16) | (data[0]<<24);
+  #ifdef LITTLE_ENDIAN
+    return v;
+  #else
+    uint8_t * data = &v;
+    return (data[3]<<0) | (data[2]<<8) | (data[1]<<16) | (data[0]<<24);
+  #endif
 }
 
 #ifdef __cplusplus
